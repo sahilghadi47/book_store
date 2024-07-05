770dcd1deaf62430189934de7ed95b62821f588d
sahilghadi47 <ghadigaonkarsahil0407@gmail.com>
2024-07-05 14:59:18 +0530

    created Input and password field

---
+++ Frontend/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Input from "./UI/InputField";
+import PasswordField from "./UI/PasswordField";
+import Register from "./Register";
+import Login from "./Login";
+import SearchBar from "./UI/SearchBar";
+export { Input, PasswordField, Register, Login, Header, Footer, SearchBar };
