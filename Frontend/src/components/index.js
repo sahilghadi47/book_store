3316efad5d7247a35caee6a2c098d188c5e90f3c
sahilghadi47 <ghadigaonkarsahil0407@gmail.com>
2024-07-06 13:30:27 +0530

    deleted components

--- Frontend/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Header from "./layout/Header";
-import Footer from "./layout/Footer";
-import Input from "./UI/InputField";
-import PasswordField from "./UI/PasswordField";
-import Register from "./Register";
-import Login from "./Login";
-import SearchBar from "./UI/SearchBar";
-export { Input, PasswordField, Register, Login, Header, Footer, SearchBar };
